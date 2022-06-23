5324ec4c5d21bcdb2ba0bf589a5c6ae6655a1f0b
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-06-23 16:15:47 +0200

    Aggiunti partials scss e importato l'header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,18 @@
 <header>
-    questo è l'header
+        <div>
+          <img style="cursor:pointer" src="{{asset('img/dc-logo.png')}}" alt="">
+        </div>
+
+        <ul>
+            <li><a href="#">characters</a></li>
+            <li><a class="active" href="#">comics</a></li>
+            <li><a href="#">movies</a></li>
+            <li><a href="#">tv</a></li>
+            <li><a href="#">games</a></li>
+            <li><a href="#">collectibles</a></li>
+            <li><a href="#">videos</a></li>
+            <li><a href="#">fans</a></li>
+            <li><a href="#">news</a></li>
+            <li><a href="#">shop</a></li>
+        </ul>
 </header>
