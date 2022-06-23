6ce92058ddc851e4d1fb07813bcdc77a0b15714e
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-06-23 22:08:41 +0200

    stampate comics card (da debuggare e migliorare il layout della hero section)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,19 @@
 
 @section('content')
 
-contenuto homepage
+<section class="hero-section">
+    <button>current series</button>
+</section>
+
+<section>
+    <div class="card-section">
+        @foreach ( $comics as $item )
+            <div class="card-element">
+                <img src="{{ $item->image }}" alt=""><br>
+                <span>{{ $item->title }}</span>
+            </div>
+        @endforeach
+    </div>
+</section>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $data = config('comics');
+
+    return view('home', ['comics'=>$data]);
 })->name('home');
